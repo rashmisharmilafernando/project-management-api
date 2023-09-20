4b38b48df4f1dcd4e8959c134370d43f2f343969
Rashmi Sharmila <108237108+rashmisharmila@users.noreply.github.com>
2023-09-20 14:37:42 +0530

    implement methods

--- src/main/java/lk/ijse/projectmanagementapi2/service/impl/ProjectBOimpl.java
+++ src/main/java/lk/ijse/projectmanagementapi2/service/impl/ProjectBOimpl.java
@@ -1,4 +1,33 @@
 package lk.ijse.projectmanagementapi2.service.impl;
 
-public class ProjectBOimpl {
+import lk.ijse.projectmanagementapi2.dto.ProjectDTO;
+import lk.ijse.projectmanagementapi2.service.ProjectBO;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class ProjectBOimpl implements ProjectBO {
+
+    @Override
+    public ProjectDTO savePriject(ProjectDTO projectDTO) {
+        return null;
+    }
+
+    @Override
+    public void updateProjectDetails(String projectid, ProjectDTO projectDTO) {
+
+    }
+
+    @Override
+    public void deleteProject(String projectid) {
+
+    }
+
+    @Override
+    public List<ProjectDTO> getAllProjectsDetails() {
+        return null;
+    }
 }
--- src/main/java/lk/ijse/projectmanagementapi2/service/impl/TechLedBOimpl.java
+++ src/main/java/lk/ijse/projectmanagementapi2/service/impl/TechLedBOimpl.java
@@ -1,4 +1,34 @@
 package lk.ijse.projectmanagementapi2.service.impl;
 
-public class TechLedBOimpl {
+import lk.ijse.projectmanagementapi2.dto.TechLeadDTO;
+import lk.ijse.projectmanagementapi2.service.TechLedBO;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class TechLedBOimpl implements TechLedBO {
+
+
+    @Override
+    public TechLeadDTO saveTechlead(TechLeadDTO techLeadDTO) {
+        return null;
+    }
+
+    @Override
+    public void updateTechLeadDetails(String techleadid, TechLeadDTO techLeadDTO) {
+
+    }
+
+    @Override
+    public void deleteTechLead(String techleadid) {
+
+    }
+
+    @Override
+    public List<TechLeadDTO> getAllTechLeadDetails() {
+        return null;
+    }
 }
