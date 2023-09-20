dc537ec5b29aea97e7a15e51c9467dc9d015cbb4
Rashmi Sharmila <108237108+rashmisharmila@users.noreply.github.com>
2023-09-20 21:21:29 +0530

    Implement Handler Methods

--- src/main/java/lk/ijse/projectmanagementapi2/controller/ProjectController.java
+++ src/main/java/lk/ijse/projectmanagementapi2/controller/ProjectController.java
@@ -1,13 +1,17 @@
 package lk.ijse.projectmanagementapi2.controller;
 
 import lk.ijse.projectmanagementapi2.dto.ProjectDTO;
+import lk.ijse.projectmanagementapi2.dto.TechLeadDTO;
 import lk.ijse.projectmanagementapi2.repo.ProjectRepo;
 import lk.ijse.projectmanagementapi2.service.ProjectBO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v1/projects")
@@ -16,19 +20,49 @@ public class ProjectController {
     ProjectBO projectBO;
 
     @GetMapping
-    public ResponseEntity<List<ProjectRepo>> getAllProjectDetails(){
-        return null;
+    public ResponseEntity<List<ProjectDTO>> getAllProjectDetails() {
+        List<ProjectDTO> projectDTOList = projectBO.getAllProjectsDetails().stream().map(project ->
+                new ProjectDTO(
+                        project.getProid(),
+                        project.getName(),
+                        project.getDescription(),
+                        project.getStartDate()
+                )
+        ).collect(Collectors.toList());
+        return new ResponseEntity<>(projectDTOList, HttpStatus.OK);
     }
+
     @PostMapping
-    public ProjectDTO saveProject(){
-        return null;
+    public ProjectDTO saveProject(
+            @RequestParam String projectId,
+            @RequestParam String projectname,
+            @RequestParam String projectDescription,
+            @RequestParam Date projectStartDate
+    ) {
+        if (projectId == null || projectId.matches("[P][0-9]{3}")) {
+            throw new RuntimeException("Invalid Project Id.Please enter the Project Id again..");
+        } else if (projectname == null || projectname.matches("[A-Za-z]")) {
+            throw new RuntimeException("Project name is empty...!");
+        } else if (projectDescription == null) {
+            throw new RuntimeException("Project description is empty...!");
+        } else if (projectStartDate == null) {
+            throw new RuntimeException("Project Start date is empty..1");
+        }
+        ProjectDTO projectDTO = new ProjectDTO();
+        projectDTO.setProid(projectId);
+        projectDTO.setName(projectname);
+        projectDTO.setDescription(projectDescription);
+        projectDTO.setStartDate(projectStartDate);
+        return projectBO.savePriject(projectDTO);
     }
-    @PutMapping
-    public void updateProjectDetails(@PathVariable String projectid,@RequestBody ProjectDTO projectDTO){
 
+    @PutMapping("projectid")
+    public void updateProjectDetails(@PathVariable String projectid, @RequestBody ProjectDTO projectDTO) {
+        projectBO.updateProjectDetails(projectid, projectDTO);
     }
-    @DeleteMapping
-    public void deleteProject(String projectid){
 
+    @DeleteMapping(params = "projectid")
+    public void deleteProject(String projectid) {
+        projectBO.deleteProject(projectid);
     }
 }
