d098d44e96b4a3533c1baedc6965724768acb05f
Rashmi Sharmila <108237108+rashmisharmila@users.noreply.github.com>
2023-09-20 14:35:52 +0530

    Add the abstract method

--- src/main/java/lk/ijse/projectmanagementapi2/service/ProjectBO.java
+++ src/main/java/lk/ijse/projectmanagementapi2/service/ProjectBO.java
@@ -1,4 +1,16 @@
 package lk.ijse.projectmanagementapi2.service;
 
+import lk.ijse.projectmanagementapi2.dto.ProjectDTO;
+
+import java.util.List;
+
 public interface ProjectBO {
+
+    ProjectDTO savePriject(ProjectDTO projectDTO);
+
+    void updateProjectDetails(String projectid,ProjectDTO projectDTO);
+
+    void deleteProject(String projectid);
+
+    List<ProjectDTO> getAllProjectsDetails();
 }
--- src/main/java/lk/ijse/projectmanagementapi2/service/TechLedBO.java
+++ src/main/java/lk/ijse/projectmanagementapi2/service/TechLedBO.java
@@ -1,4 +1,16 @@
 package lk.ijse.projectmanagementapi2.service;
 
+import lk.ijse.projectmanagementapi2.dto.TechLeadDTO;
+
+import java.util.List;
+
 public interface TechLedBO {
+
+    TechLeadDTO saveTechlead(TechLeadDTO techLeadDTO);
+
+    void updateTechLeadDetails(String techleadid,TechLeadDTO techLeadDTO);
+
+    void deleteTechLead(String techleadid);
+
+    List<TechLeadDTO> getAllTechLeadDetails();
 }
