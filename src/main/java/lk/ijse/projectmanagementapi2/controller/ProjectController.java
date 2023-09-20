f4d0d6ff3a025d5eeda28a880cb98075ba207a48
Rashmi Sharmila <108237108+rashmisharmila@users.noreply.github.com>
2023-09-20 14:40:01 +0530

    Updated the controller classes

--- src/main/java/lk/ijse/projectmanagementapi2/controller/ProjectController.java
+++ src/main/java/lk/ijse/projectmanagementapi2/controller/ProjectController.java
@@ -1,4 +1,34 @@
 package lk.ijse.projectmanagementapi2.controller;
 
+import lk.ijse.projectmanagementapi2.dto.ProjectDTO;
+import lk.ijse.projectmanagementapi2.repo.ProjectRepo;
+import lk.ijse.projectmanagementapi2.service.ProjectBO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/projects")
 public class ProjectController {
+    @Autowired
+    ProjectBO projectBO;
+
+    @GetMapping
+    public ResponseEntity<List<ProjectRepo>> getAllProjectDetails(){
+        return null;
+    }
+    @PostMapping
+    public ProjectDTO saveProject(){
+        return null;
+    }
+    @PutMapping
+    public void updateProjectDetails(@PathVariable String projectid,@RequestBody ProjectDTO projectDTO){
+
+    }
+    @DeleteMapping
+    public void deleteProject(String projectid){
+
+    }
 }
--- src/main/java/lk/ijse/projectmanagementapi2/controller/TechLedController.java
+++ src/main/java/lk/ijse/projectmanagementapi2/controller/TechLedController.java
@@ -1,4 +1,36 @@
 package lk.ijse.projectmanagementapi2.controller;
 
+import lk.ijse.projectmanagementapi2.dto.TechLeadDTO;
+import lk.ijse.projectmanagementapi2.repo.TechLedRepo;
+import lk.ijse.projectmanagementapi2.service.TechLedBO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/techlead")
 public class TechLedController {
+
+    @Autowired
+    TechLedBO techLedBO;
+
+    @PostMapping
+    public TechLeadDTO saveTechLead(){
+        return null;
+    }
+    @GetMapping
+    public ResponseEntity<List<TechLedRepo>> getAllTechLeadDetails(){
+        return null;
+    }
+    @PutMapping
+    public void updateTechLeadDetails(String techleadid,@RequestBody TechLeadDTO techLeadDTO){}
+    @DeleteMapping
+    public void deleteTechLead(String id){}
+
+
+
+
+
 }
